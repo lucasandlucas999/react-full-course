7f1a9fa3ea3bde5522194433a6cd8ab2560da7a0
Lucas <contactolucasacuna@gmail.com>
2025-01-02 13:53:02 -0300

    First commit

---
+++ 00-hola-mundo/src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+
+function App(){
+    return(
+        <article>
+            <header>
+                <img src="https://unavatar.io/lucasandlucas999" alt="avatar Lucas and Lucas" />
+                <div>
+                    <strong>Lucas Acuña</strong>
+                    <span>@lucasandlucas999</span>
+                </div>
+            </header>
+            <aside>
+                <button>
+                    Seguir
+                </button>
+            </aside>
+        </article>
+    )
+}
+
+export default App
