fd9cfa811b3afbbd32192e6934d6de7f79c3b348
Lucas <contactolucasacuna@gmail.com>
2025-01-03 08:42:31 -0300

    Dia 01-03-2025

--- 00-hola-mundo/src/App.jsx
+++ 00-hola-mundo/src/App.jsx
@@ -1,16 +1,15 @@
 import './App.css'
 import { FollowCard } from './FollowCard'
 
-function App(){
-    const atUserName = (userName) => `@${userName}`
-    return( 
-     <section className = 'sectionApp'>
-        <FollowCard atUserName={atUserName} name='Lucas' userName='lucasandlucas999' isFollow={true}></FollowCard>
-        <FollowCard atUserName={atUserName} name='Lucas' userName='lucasandlucas999' isFollow={false}></FollowCard>
-        <FollowCard atUserName={atUserName} name='Lucas' userName='lucasandlucas999' isFollow={false}></FollowCard>
-        <FollowCard atUserName={atUserName} name='Lucas' userName='lucasandlucas999' isFollow={false}></FollowCard>
-     </section>    
-     )
+function App() {
+    return (
+        <section className='sectionApp'>
+            <FollowCard userName='lucasandlucas999' isFollowing={true}>Dedo</FollowCard>
+            <FollowCard userName='elonmusk' isFollowing={true}>Elon Musk</FollowCard>
+            <FollowCard userName='torvalds' isFollowing={true}>Linus Torvalds</FollowCard>
+        </section>
+    )
+
 }
 
 export default App
--- 00-hola-mundo/src/FollowCard.jsx
+++ 00-hola-mundo/src/FollowCard.jsx
@@ -1,22 +1,27 @@
-
-export function FollowCard({ atUserName, name, userName, isFollow }) {
-    const imgSource = `https://unavatar.io/${userName}`
-    const imgAlt = `${userName} image`
-    return (
-        <article className='tw-followCard'>
-            <header className='tw-followCard-header'>
-                <img className='tw-followCard-avatar' src={imgSource}
-                    alt={imgAlt} />
-                <div className='tw-followCard-info'>
-                    <strong>{name}</strong>
-                    <span className="tw-followCard-userName">{atUserName(userName)}</span>
+export function FollowCard({userName, children, isFollowing}){
+    const imgSrc = `https://unavatar.io/${userName}`
+    const atUserName = (userName) => `@${userName}`
+    const imgAlt = `${atUserName} image`
+    return(
+        <article className="tw-followCard">
+            <header className="tw-followCard-header">
+            <img className='tw-followCard-avatar' src={imgSrc} alt={imgAlt} />
+                <div className="tw-followCard-info">
+                    <strong>
+                        {children}
+                    </strong>
+                    <span className="tw-followCard-userName">
+                        {atUserName(userName)}
+                    </span>
                 </div>
             </header>
-            <button className="tw-followCard-button">
-                {(isFollow) ? 'Siguiendo' : 'Seguir'}
-            </button>
+            <aside>
+                <button className="tw-followCard-button">
+                    {(isFollowing)
+                    ? 'Siguiendo'
+                    : 'Seguir'}
+                </button>
+            </aside>
         </article>
     )
-
 }
-
